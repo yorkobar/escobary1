6c2b9d80478da3348fb1a4063432932e9e618d18
Yorman Escobar <escobary1@gauss.acsl.newpaltz.edu>
2018-08-30 18:00:30 -0400

    initial commit

---
+++ hello world.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   // printf() displays the string inside quotation
+   cout << "Hello, world!" << std::endl;
+   return 0;
+}
