73d66929e331f925def49a6650769e77bc2fb95a
Yorman Escobar <escobary1@gauss.acsl.newpaltz.edu>
2018-09-12 18:11:33 -0400

    initial commit

---
+++ adding.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+	int a  = 12;
+	int b  = 5;
+
+	int c;
+	c = a + b ;
+	cout << "A + B: " << c << "\n"; // anything between the "" will be a string
+   return 0;
+}
